7cc09d33a2a5b382d6c06ed520bcd99257787a69
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-17 13:48:32 +0200

    schema is inserted

--- sql-practice/schema.sql
+++ sql-practice/schema.sql
@@ -1,13 +1,16 @@
-DROP table if exists customer;
+DROP table if exists coffee_orders;
 
-DROP table if exists coffee;
+DROP TABLE if EXISTS coffee;
 
-DROP table if exists orders;
+DROP TABLE if EXISTS customers;
 
-CREATE table customer(
+CREATE table customers(
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name varchar(100) NOT NULL,
-  points INTEGER DEFAULT 5
+  name varchar(40) NOT NULL,
+  phone NUMERIC(10, 0) UNIQUE,
+  email varchar(255) UNIQUE,
+  points INTEGER DEFAULT 5,
+  created_at TIMESTAMP NOT NULL
 );
 
 CREATE table coffee(
@@ -15,8 +18,10 @@ CREATE table coffee(
   name varchar(250) NOT NULL
 );
 
-CREATE TABLE orders(
+CREATE TABLE coffee_orders(
   customer_id INTEGER,
   coffee_id INTEGER,
-  FOREIGN KEY (customer_id) REFERENCES customer(id) FOREIGN KEY (coffee_id) REFERENCES coffee(id)
+  is_redeemed vrachr(100) DEFAULT "not redeemed",
+  ordered_at TIMESTAMP,
+  FOREIGN KEY (ordered_at) REFERENCES customers(created_at) FOREIGN KEY (customer_id) REFERENCES customers(id) FOREIGN KEY (coffee_id) REFERENCES coffee(id)
 );
