fbcb817bf707d651874a4d393eba0247a54843e4
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-06 18:09:56 +0200

    phase 2

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,38 @@
 -- Your code here
+--  Creating tables and Seeding the coffe Table 
+--  Droping the Previous DB
+DROP table if exists coffee_orders;
+
+DROP TABLE if EXISTS coffee;
+
+DROP TABLE if EXISTS customers;
+
+CREATE table customers(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name varchar(40) NOT NULL,
+  phone NUMERIC(10, 0) UNIQUE,
+  email varchar(255) UNIQUE,
+  points INTEGER DEFAULT 5,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE table coffee(
+  id INTEGER PRIMARY key AUTOINCREMENT,
+  name varchar(250) NOT NULL
+);
+
+CREATE TABLE coffee_orders(
+  customer_id INTEGER,
+  coffee_id INTEGER,
+  is_redeemed NUMERIC(1, 0) DEFAULT 0,
+  ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (coffee_id) REFERENCES coffee(id) FOREIGN KEY (customer_id) REFERENCES customers(id) FOREIGN KEY (coffee_id) REFERENCES coffee(id)
+);
+
+-- seeding the coffee table 
+INSERT INTO
+  coffee (name)
+VALUES
+  ("coffee regular"),
+  ("coffee with milk"),
+  ("Esspersso coffee");
--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,15 +1,75 @@
-IF EXISTS (SELECT * from customers WHERE points > 10 AND name ='Rachel')
-	
-      name = "Rachel"
-  ) - 10
-WHERE
-  name = "Rachel";
-ELSE
-	INSERT INTO coffee_orders (customer_id, coffee_id) VALUES (
-	SELECT id from customers WHERE name ="Rachel",3
-	)
-	UPDATE
-      name = "Rachel"
-  ) + 2
-WHERE
-  name = "Rachel";
+-- events 
+--  1 
+/*
+ INSERT INTo
+ customers (name, phone)
+ VALUES
+ ("Rachel", 111111111);
+ */
+-- 2
+/*
+ SELECT
+ id,
+ points CASE
+ points
+ WHEN points >= 10 THEN
+ INSERT INTO
+ coffee_orders (customer_id, coffee_id, is_redeemed)
+ VALUES
+ (id, 2, 1)
+ ELSE
+ INSERT INTO
+ coffee_orders (customer_id, coffee_id, is_redeemed)
+ VALUES
+ (id, 2, 0)
+ END
+ FROM
+ customers
+ WHERE
+ name = "Rachel";
+ */
+IIF(
+  SELECT
+    points
+  FROM
+    customers
+  WHERE
+    name = "Rachel" >= 10,
+  INSERT INTO
+    coffee_orders (customer_id, coffee_id.is_redeemed)
+  VALUES
+    (
+      SELECT
+        id
+      FROM
+        customers
+      WHERE
+        name = "Rachel",
+        2,
+        1
+    ),
+  INSERT INTO
+    coffee_orders (customer_id, coffee_id.is_redeemed)
+  VALUES
+    (
+      SELECT
+        id
+      FROM
+        customers
+      WHERE
+        name = "Rachel",
+        2,
+        0
+    )
+) -- 3
+/*
+ INSERT INTo
+ customers (name, email, phone)
+ VALUES
+ ("Monica", "monica @friends.show", 222222222);
+ 
+ INSERT INTo
+ customers (name, email, phone)
+ VALUES
+ ("Phoebe", "phoebe @friends.show", 333333333);
+ */
