f5e90611234817226cb20d53112b593760cb56f8
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-10-08 13:43:12 +0200

    starting again

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -1,75 +1,88 @@
 -- events 
 --  1 
-/*
- INSERT INTo
- customers (name, phone)
- VALUES
- ("Rachel", 111111111);
- */
+INSERT INTo
+  customers (name, phone)
+VALUES
+  ("Rachel", 111111111);
+
 -- 2
-/*
- SELECT
- id,
- points CASE
- points
- WHEN points >= 10 THEN
- INSERT INTO
- coffee_orders (customer_id, coffee_id, is_redeemed)
- VALUES
- (id, 2, 1)
- ELSE
- INSERT INTO
- coffee_orders (customer_id, coffee_id, is_redeemed)
- VALUES
- (id, 2, 0)
- END
- FROM
- customers
- WHERE
- name = "Rachel";
- */
-IIF(
-  SELECT
-    points
-  FROM
-    customers
-  WHERE
-    name = "Rachel" >= 10,
-  INSERT INTO
-    coffee_orders (customer_id, coffee_id.is_redeemed)
-  VALUES
-    (
-      SELECT
-        id
-      FROM
-        customers
-      WHERE
-        name = "Rachel",
-        2,
-        1
-    ),
-  INSERT INTO
-    coffee_orders (customer_id, coffee_id.is_redeemed)
-  VALUES
-    (
-      SELECT
-        id
-      FROM
-        customers
-      WHERE
-        name = "Rachel",
-        2,
-        0
-    )
-) -- 3
-/*
- INSERT INTo
- customers (name, email, phone)
- VALUES
- ("Monica", "monica @friends.show", 222222222);
- 
- INSERT INTo
- customers (name, email, phone)
- VALUES
- ("Phoebe", "phoebe @friends.show", 333333333);
- */
+-- check the current points of the customer
+SELECT
+  points
+from
+  customers
+WHERE
+  name = "Rachel";
+
+--  update the customer's points
+UPDATE
+  customers
+SET
+  points = 6
+WHERE
+  name = "Rachel";
+
+-- then add the coffee order
+INSERT INTO
+  coffee_orders (coffee_id, customer_id, is_redeemed)
+VALUES
+  (3, 1, 0);
+
+--  3
+--  Two new customers joined the loyalty program
+INSERT INTo
+  customers (name, email, phone)
+VALUES
+  ("Monica", "monica @friends.show", 222222222);
+
+INSERT INTo
+  customers (name, email, phone)
+VALUES
+  ("Phoebe", "phoebe @friends.show", 333333333);
+
+--   4 Phoebe purchases three coffees
+-- first coffe 
+-- check the current points of the customer
+SELECT
+  points
+from
+  customers
+WHERE
+  name = "Phoebe";
+
+--  update the customer's points
+UPDATE
+  customers
+SET
+  points = 6
+WHERE
+  name = "Phoebe";
+
+-- then add the coffee order
+INSERT INTO
+  coffee_orders (coffee_id, customer_id, is_redeemed)
+VALUES
+  (1, 3, 0);
+
+--  second coffe 
+-- check the current points of the customer
+SELECT
+  points
+from
+  customers
+WHERE
+  name = "Phoebe";
+
+--  update the customer's points
+UPDATE
+  customers
+SET
+  points = 6
+WHERE
+  name = "Phoebe";
+
+-- then add the coffee order
+INSERT INTO
+  coffee_orders (coffee_id, customer_id, is_redeemed)
+VALUES
+  (1, 3, 0);
